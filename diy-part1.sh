f6d8d1a269f76622fa2e594a812458a394a35ad2
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2024-11-16 01:33:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,21 @@
 
 #sed用法里面，\t代表一个tab制表符，也就是缩进。
 #自行编译时，会出现内核的魔法值不一样，需要完成如下修改。这样的话在系统里更新软件包时就不会产生错误。
+#方式一：
+
+#执行以下脚本即可写入到 vermagic 文件中。注意： 23.05-SNAPSHOT 为版本号
+#curl -s https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/targets/rockchip/armv8/immortalwrt-23.05-snapshot-r28094-a8d3d10f4c-rockchip-armv8.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+
+#方式二：
+
+#打开官方对应版本的下载链接：https://downloads.immortalwrt.org/releases
+#点击：23.05-SNAPSHOT -> targets -> rockchip -> armv8
+#下载 Supplementary Files 中的 immortalwrt-23.05-snapshot-r28094-a8d3d10f4c-rockchip-armv8.manifest 文件
+#使用文本编辑器打开该文件，记录下 kernel 最后面的那串字符串
+#执行以下脚本写入到 vermagic 文件中
+# 注意：f927e86044686b2ce4b806f436e32ca1 就是上文中的字符串
+#echo 'f927e86044686b2ce4b806f436e32ca1' > vermagic
+#这里我们用方式二
 echo 'f927e86044686b2ce4b806f436e32ca1' > vermagic
 
 #在121行前面加一个#将这行注释掉
