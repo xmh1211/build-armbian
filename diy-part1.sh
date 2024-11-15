315a65c6ad328da7edda0e5041ecf13b0fd6dfc0
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2024-11-15 21:48:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'f927e86044686b2ce4b806f436e32ca1' > vermagic
+sed -i 's/grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic/#grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic/g' include/kernel-defaults.mk
+sed -i '121 a cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
+sed -i 's/set system.@system[-1].hostname='ImmortalWrt'/set system.@system[-1].hostname='X-WRT'/g' package/base-files/files/bin/config_generate
+sed -i 's/set system.@system[-1].timezone='UTC'/set system.@system[-1].timezone='CST-8'/g' package/base-files/files/bin/config_generate
+sed -i '315 a set system.@system[-1].zonename='Asia/Shanghai' package/base-files/files/bin/config_generate
+sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"} ;;/lan) ipad=${ipaddr:-"192.168.100.1"} ;;/g' package/base-files/files/bin/config_generate
