9c48e5102591abcf9e723812f7e7df8e07fa8089
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2024-11-15 23:51:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,16 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'f927e86044686b2ce4b806f436e32ca1' > vermagic
-
-sed -i '121 s/grep/#grep/g' include/kernel-defaults.mk
-
-awk 'NR==121 {print; print "        cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic'\''"; next} 1' include/kernel-defaults.mk > tmp && mv tmp include/kernel-defaults.mk
-
-sed -i 's/ImmortalWrt/X-WRT/g' package/base-files/files/bin/config_generate
-
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-
-awk 'NR==316 {print; print "                set system.@system[-1].zonename='\''Asia/Shanghai'\''"; next} 1' package/base-files/files/bin/config_generate > tmp && mv tmp package/base-files/files/bin/config_generate
-
-sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"} ;;/lan) ipad=${ipaddr:-"192.168.100.1"} ;;/g' package/base-files/files/bin/config_generate
